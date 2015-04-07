4902b31414654c32e30290ea8625f225dea7a296
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-04-07 17:18:49 -0600

    Initial Commit

---
+++ chesterfield_upcoming_events.php
@@ -0,0 +1,29 @@
+<?php defined( 'ABSPATH' ) or die( 'No longer allowed at the Chesterfield' );
+/**
+ * @package chesterfield_upcoming_events
+ */
+
+/*
+Plugin Name: Chesterfield Upcoming Events
+Description: Display upcoming events in widget
+Version: 0.0.1
+Author: Zach Metcalf
+License: GPLv2 or later
+*/
+
+/*  Copyright 2015  Zach Metcalf
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
